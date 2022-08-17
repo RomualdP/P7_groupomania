340cfe7558114599dcd816fcdb2f5bd8dc689742
Romuald Piquet <romuald.piquet@gmail.com>
2022-08-17 11:21:37 +0200

    correcting nodemon issue

--- frontend/src/page/PageNoteFound.jsx
+++ frontend/src/page/PageNoteFound.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function PageNoteFound() {
   return (
-    <div>
+    <div className="pagenotfound--container">
+        <img src="" alt="" />
       <h1>404 Error</h1>
       <h1>Page Not Found</h1>
     </div>
